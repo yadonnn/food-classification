4c921edebacb24bb7b45cec4e7ab2141d7972ae4
lee <lys453@gmail.com>
2026-03-03 23:22:30 +0900

    fix: 잘못된 경로, 디렉토리 재생성 로직 변경

--- image-prep-pipeline/config.py
+++ image-prep-pipeline/config.py
@@ -14,31 +14,34 @@ DATA_DIR = BASE_DIR / "data"
 # --- AIHub 다운로더 설정 ---
 # ======================================================================
 AIHUB_API_KEY = os.getenv("AIHUB_API_KEY")
-DOWNLOAD_DST_DIR = DATA_DIR / "tmp" / "raw"
+DOWNLOAD_DST_DIR = DATA_DIR / "tmp" / "src"
 AIHUB_PROJECT_KEY = "242"
 AIHUB_FILE_KEYS = [
     "49602", "49603", "49520", "49521", "49522", "49523", "49524", "49525", "49526", "49527", "49528", "49529", "49530", # 원천(Img)
     "49589", "49590", "49591", "49592", "49593", "49594", "49595", "49596", "49597", "49598", "49599", "49600", "49601"  # 라벨(Json)
 ]
 
-# ====================================================================== 
+# ======================================================================
 # --- 압축파일 경로 설정 ---
-# ====================================================================== 
+# ======================================================================
 """다운로드 이후 압축파일 경로 설정"""
 EXTRACT_SRC_DIR = DOWNLOAD_DST_DIR
 EXTRACT_DST_DIR = DATA_DIR / "tmp" / "extracted"
 
-"""전처리 이후 압축파일 경로 설정"""
-ARCHIVE_SRC_DIR = EXTRACT_DST_DIR
-ARCHIVE_DST_DIR = DATA_DIR / "tmp" / "archive"
-
 # ====================================================================== 
 # --- 이미지 전처리 설정 ---
 # ====================================================================== 
-TRANSFORM_SRC_DIR = ARCHIVE_DST_DIR
-TARGET_SIZE = (384, 384)
+TRANSFORM_SRC_DIR = EXTRACT_DST_DIR
+TARGET_SIZE = 384
 TRANSFORM_EXTENSION = "webp"
 TRANSFORM_DST_DIR = DATA_DIR / "tmp" / f"{TRANSFORM_EXTENSION}_{TARGET_SIZE}"
+
+# ======================================================================
+# --- 압축파일 경로 설정 ---
+# ======================================================================
+"""전처리 이후 압축파일 경로 설정"""
+ARCHIVE_SRC_DIR = TRANSFORM_DST_DIR
+ARCHIVE_DST_DIR = DATA_DIR / "tmp" / "archive"
 
 # ====================================================================== 
 # --- GCS 버킷 설정 ---
@@ -55,6 +58,5 @@ def init_directories():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
     DOWNLOAD_DST_DIR.mkdir(parents=True, exist_ok=True)
     EXTRACT_DST_DIR.mkdir(parents=True, exist_ok=True)
-    TRANSFORM_SRC_DIR.mkdir(parents=True, exist_ok=True)
     TRANSFORM_DST_DIR.mkdir(parents=True, exist_ok=True)
-    ARCHIVE_DIR.mkdir(parents=True, exist_ok=True)
+    ARCHIVE_DST_DIR.mkdir(parents=True, exist_ok=True)
--- image-prep-pipeline/main.py
+++ image-prep-pipeline/main.py
@@ -18,17 +18,17 @@ def get_zip_files(directory: Path) -> set:
 
 
 def main():
-	# 폴더 생성
-	config.init_directories()
 	# 파일별 다운로드 -> 압축 해제 -> 변환 -> 적재 -> 정리 루프
 	for file_key in config.AIHUB_FILE_KEYS:
-	 # 1. aihub 다운로드
-		command = f"aihubshell -mode d -datasetkey {config.AIHUB_PROJECT_KEY} -filekey {file_key} -aihubapikey {config.AIHUB_API_KEY}"
-		subprocess.run(command, cwd=config.DOWNLOAD_DIR, shell=True, check=True)
+		# 폴더 생성
+		config.init_directories()
+		# 1. aihub 다운로드
+		# command = f"aihubshell -mode d -datasetkey {config.AIHUB_PROJECT_KEY} -filekey {file_key} -aihubapikey {config.AIHUB_API_KEY}"
+		# subprocess.run(command, cwd=config.DOWNLOAD_DST_DIR, shell=True, check=True)
 
 		# 2. 압축 해제
-		print(get_zip_files(config.DOWNLOAD_DIR))
-		file_paths = get_zip_files(config.DOWNLOAD_DIR)
+		print(get_zip_files(config.DOWNLOAD_DST_DIR))
+		file_paths = get_zip_files(config.DOWNLOAD_DST_DIR)
 		image_paths = list()
 		for file_path in file_paths:
 			with zipfile.ZipFile(file_path, 'r') as z:
@@ -38,7 +38,7 @@ def main():
 					encoded_name = original_name.encode('cp437').decode('cp949')
 					print(encoded_name)
 
-					target_path = config.EXTRACT_DIR / encoded_name
+					target_path = config.EXTRACT_DST_DIR / encoded_name
 					if member.is_dir():
 						target_path.mkdir(parents=True, exist_ok=True)
 						continue
@@ -56,20 +56,20 @@ def main():
 			print(img.shape)
 			img = cv2.resize(img, (config.TARGET_SIZE, config.TARGET_SIZE))
 
-			relative_path = image_path.relative_to(config.EXTRACT_DIR)
+			relative_path = image_path.relative_to(config.EXTRACT_DST_DIR)
 			save_path = config.TRANSFORM_DST_DIR / relative_path.with_suffix(f".{config.TRANSFORM_EXTENSION}")
 			save_path.parent.mkdir(parents=True, exist_ok=True)
 			print(f"save_path: {save_path}")
 			cv2.imwrite(save_path, img, [cv2.IMWRITE_WEBP_QUALITY, 90])
 
 		# 4. 전처리된 이미지 압축파일 로컬 저장
-		shutil.make_archive(config.ARCHIVE_DIR / file_key, 'zip', config.TRANSFORM_DST_DIR)
+		shutil.make_archive(config.ARCHIVE_DST_DIR / file_key, 'zip', config.ARCHIVE_SRC_DIR)
 
 		# 5. 원본 파일 삭제
 		shutil.rmtree(config.DOWNLOAD_DST_DIR) 	# 1. 다운로드 zip 경로
 		shutil.rmtree(config.EXTRACT_DST_DIR)  	# 2. 압축 해제 경로
 		shutil.rmtree(config.TRANSFORM_DST_DIR) # 3. 전처리된 이미지 경로
-		# shutil.rmtree(config.ARCHIVE_DIR) 	# 4. 전처리된 이미지 압축파일 경로(GCS 업로드 후 삭제 예정)
+		# shutil.rmtree(config.ARCHIVE_DST_DIR) 	# 4. 전처리된 이미지 압축파일 경로(GCS 업로드 후 삭제 예정)
 
 if __name__ == "__main__":
 	main()
