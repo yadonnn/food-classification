9258ba793d8c2ab475502a908624534b29656f38
yadonnn <122248976+yadonnn@users.noreply.github.com>
2026-03-06 04:18:25 +0900

    feat: 분기 생성 후 라벨 데이터 처리 로직 추가 (#8)

--- image_pipeline/main.py
+++ image_pipeline/main.py
@@ -24,19 +24,30 @@ def run_pipeline(file_key: str, key_map: dict[str, tuple[str, str]]):
 	"""파일별 다운로드 -> 압축 해제 -> 변환 -> 적재 -> 정리 루프"""
 	config.init_directories()
 	file_name, _ = key_map[file_key]
+
 	# 1. 다운로드
 	file_path = downloader.download_file(file_key, file_name)
 	print(f"file_key: {file_key} 다운로드 완료")
-	# 2. 압축 해제
+
+	# 2. 압축 해제 분기(json 파일일 경우, 이동만 하고 종료)
+	if "json" in file_name:
+		json_file_path = preprocessor.move_file(file_path)
+		print(f"file_name: {file_name} json 파일 이동 완료")
+		return
+	
+	# 2-1. 이미지 파일 압축 해제
 	image_path_list = preprocessor.extract_archive(file_path)
 	print(f"file_key: {file_key} 압축 해제 완료")
+	
 	# 3. 이미지 변환
 	for image_path in image_path_list:
 		preprocessor.resize_image(image_path)
 	print(f"file_key: {file_key} 이미지 변환 완료")
+	
 	# 4. 전처리된 이미지 압축파일 로컬 저장
 	preprocessor.make_archive(file_name)
 	print(f"file_key: {file_key} 압축파일 로컬 저장 완료")
+	
 	# 5. 원본 파일 삭제
 	shutil.rmtree(config.DOWNLOAD_DST_DIR) 	# 1. 다운로드 zip 경로
 	shutil.rmtree(config.EXTRACT_DST_DIR)  	# 2. 압축 해제 경로
@@ -45,6 +56,5 @@ def run_pipeline(file_key: str, key_map: dict[str, tuple[str, str]]):
 
 if __name__ == "__main__":
 	key_map = make_key_map_from_manifest()
-	print(key_map["49525"])
-	for file_key in ["49525", "49526"]:
+	for file_key in ["49589", "49526"]:
 		run_pipeline(file_key, key_map)
